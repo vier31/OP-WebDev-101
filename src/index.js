2cce7b2f32a5f357a96fc5b8ead2a60e32b04bfb
Jan Schröder <34372068+vier31@users.noreply.github.com>
2020-10-13 00:05:37 +0200

    basic game

--- src/index.js
+++ src/index.js
@@ -1,10 +1,85 @@
 import "./styles.css";
 
+function playerSays(pick) {
+  const computerPick = computerPlay();
+  document.getElementById(
+    "computer"
+  ).innerText = `Computer says: ${computerPick}`;
+  document.getElementById("player").innerText = `Player says: ${pick}`;
+  const game = playRound(computerPick, pick);
+  document.getElementById("result").innerText = `${game}`;
+}
+
+function computerPlay() {
+  const rollDice = Math.floor(Math.random() * Math.floor(2));
+  let play;
+  switch (rollDice) {
+    case 0:
+      play = "rock";
+      break;
+    case 1:
+      play = "paper";
+      break;
+    case 2:
+      play = "scissors";
+      break;
+    default:
+      play = "pass";
+  }
+  return play;
+}
+
+function playRound(computerSelection, playerSelection) {
+  const playerInput = playerSelection.toLowerCase();
+  let result;
+  if (computerSelection === "paper") {
+    if (playerInput === "rock") {
+      result = "Computer wins! Paper beats rock!";
+    }
+    if (playerInput === "scissors") {
+      result = "Player wins! Scissors beat paper!";
+    }
+    if (playerInput === "paper") {
+      result = "Draw! Paper & paper";
+    }
+  }
+  if (computerSelection === "scissors") {
+    if (playerInput === "rock") {
+      result = "Player wins! Rock beats scissors!";
+    }
+    if (playerInput === "scissors") {
+      result = "Draw! Scissors & scissors";
+    }
+    if (playerInput === "paper") {
+      result = "Computer wins! Scissors beat paper!";
+    }
+  }
+  if (computerSelection === "rock") {
+    if (playerInput === "rock") {
+      result = "Draw! Rock & rock!";
+    }
+    if (playerInput === "scissors") {
+      result = "Computer wins! Rock beats scissors!";
+    }
+    if (playerInput === "paper") {
+      result = "Player wins! Paper beats rock!";
+    }
+  }
+  return result;
+}
+
 document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
+<p id="computer">Computer says:</p>
+<p id="player">Player says:</p>
+<p id="result">Result:</p></br>
+<p>Press a button to play: <button id="rock">Rock</button> <button id="paper">Paper</button> <button id="scissors">Scissors</button></p>
 `;
+
+const rock = document.getElementById("rock");
+rock.onclick = () => playerSays("rock");
+
+const paper = document.getElementById("paper");
+paper.onclick = () => playerSays("paper");
+
+const scissors = document.getElementById("scissors");
+scissors.onclick = () => playerSays("scissors");
